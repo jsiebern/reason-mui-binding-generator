21a22f57b4580797bb7a13b66f7ccb91049c18d4
Jonathan Siebern <jsiebern@web.de>
2017-11-02 17:07:56 +0100

    Project Setup & Extracting Flow information from MUI Source

---
+++ scripts/extract.js
@@ -0,0 +1,59 @@
+import { mkdir, readFileSync, writeFileSync } from 'fs';
+import kebabCase from 'lodash/kebabCase';
+import * as reactDocgen from 'react-docgen';
+import getStylesCreator from '/Users/jonathansiebern/git/_fork/material-ui/src/styles/getStylesCreator';
+import createMuiTheme from '/Users/jonathansiebern/git/_fork/material-ui/src/styles/createMuiTheme';
+
+import findComponents from './find-components';
+import ensureExists from './ensure-folder-exists';
+
+const components = findComponents();
+const theme = createMuiTheme();
+const rootDirectory = path.resolve('/Users/jonathansiebern/git/_fork/material-ui');
+const outputDirectory = path.join(__dirname, '../', 'output');
+
+components.forEach(componentPath => {
+    const src = readFileSync(componentPath, 'utf8');
+
+    if (src.match(/@ignore - internal component\./)) {
+        return;
+    }
+
+    const component = require(componentPath);
+    const styles = {
+        classes: [],
+        name: null,
+    };
+
+    if (component.styles && component.default.options) {
+        // Collect the customization points of the `classes` property.
+        styles.classes = Object.keys(getStylesCreator(component.styles).create(theme)).filter(
+            className => !className.match(/^(@media|@keyframes)/),
+        );
+        styles.name = component.default.options.name;
+    }
+
+    let reactAPI;
+    try {
+        reactAPI = reactDocgen.parse(src);
+    } catch (err) {
+        console.log('Error parsing src for', componentPath);
+        throw err;
+    }
+
+    reactAPI.name = path.parse(componentPath).name;
+    reactAPI.styles = styles;
+    reactAPI.filename = componentPath.replace(rootDirectory, '');
+    reactAPI.importPath = `material-ui/${componentPath.replace(`${rootDirectory}/src/`, '').replace('.js', '')}`;
+
+    ensureExists(outputDirectory, 0o744, err => {
+        if (err) {
+            console.log('Error creating directory', outputDirectory);
+            return;
+        }
+
+        writeFileSync(path.resolve(outputDirectory, `${kebabCase(reactAPI.name)}.json`), JSON.stringify(reactAPI));
+
+        console.log('Extracted JSON for', componentPath);
+    });
+});
