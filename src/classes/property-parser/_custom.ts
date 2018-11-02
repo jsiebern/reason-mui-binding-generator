db725e7e735684fa7279da15afd70759416e4ae0
Jonathan Siebern <jsiebern88@gmail.com>
2018-11-02 15:03:25 +0100

    Updates custom extraction process

--- src/classes/property-parser/_custom.ts
+++ src/classes/property-parser/_custom.ts
@@ -3,12 +3,18 @@ import EnumFactory from './_enum';
 
 const factory = (propertyType: PropType$Custom) => {
     if (propertyType.raw != null && propertyType.raw.indexOf('PropTypes.oneOf') > -1) {
-        const match = /PropTypes\.oneOf\((['\[\]a-zA-Z0-9, ]*)\)/gm.exec(propertyType.raw);
+        const match = /PropTypes\.oneOf\(\[((.|\n)*)\]\)/gm.exec(propertyType.raw);
         if (match != null) {
-            const json: string[] = JSON.parse(match[1].replace(/'/gm, '"'))
+            const array = match[1].split(',').reduce((prev, value) => {
+                const match = /'(.*)'/gm.exec(value);
+                if (match) {
+                    return [...prev, match[1]];
+                }
+                return prev;
+            }, []);
             return EnumFactory({
                 name: 'enum',
-                value: json.map(value => ({ value, computed: false })),
+                value: array.map(value => ({ value, computed: false })),
             });
         }
     }
